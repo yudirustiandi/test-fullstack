fcf09fcccaa678b9b06fb1fd8dd7d0c02b9eaaf7
Yudi Rustiandi <yudirustiandi@gmail.com>
2020-10-23 15:02:09 +0700

    Update user seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin'),
+        ]);
     }
 }
