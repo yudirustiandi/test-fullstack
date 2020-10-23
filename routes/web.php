9451d5922dd731eb6f81f111de6127f511c79196
Yudi Rustiandi <yudirustiandi@gmail.com>
2020-10-23 13:11:03 +0700

    Update database config and route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Dashboard
+Route::get('/manage/dashboard', 'Manage\DashboardController@index');
+
+// Cars
+Route::get('/manage/car', 'Manage\CarController@index');
+Route::get('/manage/car/add', 'Manage\CarController@store');
+Route::post('/manage/car/add', 'Manage\CarController@store');
+Route::get('/manage/car/edit/{id}', 'Manage\CarController@store');
+Route::post('/manage/car/edit/{id}', 'Manage\CarController@store');
+Route::post('/manage/car/delete/{id}', 'Manage\CarController@delete');
+
+
+// Sales
+Route::get('/manage/sale', 'Manage\SaleController@index');
