8ce9cd46a2daeb4c7347d9e5043598d1ad0072b9
Yudi Rustiandi <yudirustiandi@gmail.com>
2020-10-23 15:25:05 +0700

    Update dashboard

--- resources/views/manage/dashboard.blade.php
+++ resources/views/manage/dashboard.blade.php
@@ -5,4 +5,49 @@
     <h1>{{ $title }}</h1>
 </div>
 
+<div class="panel panel-primary">
+    <div class="panel-heading">DATA HARI INI</div>
+    <div class="panel-body">
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <tr>
+                    <td>Mobil yang paling banyak dijual</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Penjualan Hari ini</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Total Penjualan Hari ini</td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+<div class="panel panel-primary">
+    <div class="panel-heading">DATA 7 MINGGU TERAKHIR</div>
+    <div class="panel-body">
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <tr>
+                    <td>Mobil yang paling banyak dijual</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Penjualan 7 hari terakhir</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Total Penjualan 7 hari terakhir</td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+
 @endsection
